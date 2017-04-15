83c734d21094ef6f11e0043667e90394186b16fd
vimmis <vimmi.swami@outlook.com>
2017-04-15 02:04:51 -0700

    addition of setters, getters

--- src/main/java/edu/sjsu/cmpe275/lab2/Plane.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/Plane.java
@@ -1,8 +1,51 @@
 package edu.sjsu.cmpe275.lab2;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Plane {
     private int capacity;
-    private String model; 
+	private String model; 
     private String manufacturer;
-    private int yearOfManufacture;
+    @Column(name = "yearofmanufacture" )
+    private int yearofmanufacture;
+    
+    public Plane(int capacity, String model, String manufacturer, int yearOfManufacture) {
+		super();
+		this.capacity = capacity;
+		this.model = model;
+		this.manufacturer = manufacturer;
+		this.yearofmanufacture = yearOfManufacture;
+	}
+    public Plane(){}
+    
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	
+	public int getYearOfManufacture() {
+		return yearofmanufacture;
+	}
+	public void setYearOfManufacture(int yearOfManufacture) {
+		this.yearofmanufacture = yearOfManufacture;
+	}
 }
